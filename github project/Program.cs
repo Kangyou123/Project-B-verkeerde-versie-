91cc6ed43abd688c6b18b1f8a22d37e168c111a3
Kangyou123 <80524495+Kangyou123@users.noreply.github.com>
2022-03-14 11:38:22 +0100

    age validation

--- github project/Program.cs
+++ github project/Program.cs
@@ -1,5 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+
+
+Console.WriteLine("Enter age:");
+string value;
+int Age;
+value = Console.ReadLine();
+Age = Convert.ToInt32(value);
+
+if (Age >= 16)
+{
+    Console.WriteLine("Ur old enough to make an account");
+}
+else
+{
+    Console.WriteLine("Ur too young to make an account");
+}

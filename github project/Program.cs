baf5669a5a8d6a4b976e69a5b53cfdfac6332919
Kangyou123 <80524495+Kangyou123@users.noreply.github.com>
2022-03-27 22:16:31 +0200

    nummer mislukt gefixt

--- github project/Program.cs
+++ github project/Program.cs
@@ -6,25 +6,18 @@ namespace Menu
     {
         public static void Main(string[] args)
         {
-            Begin: Console.WriteLine("Hallo! \n");
+        Begin: Console.WriteLine("Hallo! \n");
             Console.WriteLine(" Kies 1 voor de Voorgerechten");
             Console.WriteLine(" Kies 2 voor de Hoofdgerechten");
             Console.WriteLine(" Kies 3 voor de Vegatarische gerechten");
             Console.WriteLine(" Kies 4 voor de Dessert \n ");
-            dot: Console.WriteLine("\n");
+        dot: Console.WriteLine("\n");
 
             Console.WriteLine(" Type een nummer in : ");
             int Choose_nummer = int.Parse(Console.ReadLine());
 
-            if (Choose_nummer != 1 || Choose_nummer != 2 || Choose_nummer != 3 || Choose_nummer != 4)
-            {
-                Console.WriteLine("\n  Nummer mislukt,  Kies tussen de nummers 1 - 2 - 3 - 4");
-                goto dot;
 
-
-
-            }
-            else if (Choose_nummer == 1)
+            if (Choose_nummer == 1)
             {
                 Console.Clear();
                 Console.WriteLine("Gerecht 1        Prijs :3.5 ");
@@ -71,6 +64,11 @@ namespace Menu
                 Console.WriteLine("Gerecht 4");
                 Console.WriteLine("Gerecht 5");
                 Console.WriteLine("Gerecht 6");
+            }
+            else
+            {
+                Console.WriteLine("\n  Nummer mislukt,  Kies tussen de nummers 1 - 2 - 3 - 4");
+                goto dot;
             }
         }
     }

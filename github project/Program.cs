67aa4965d6c94090f245b10b0a72b228c072f86e
Kangyou123 <80524495+Kangyou123@users.noreply.github.com>
2022-03-28 00:29:54 +0200

    Error gefixed

--- github project/Program.cs
+++ github project/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Menu
 {
@@ -14,62 +18,117 @@ namespace Menu
         dot: Console.WriteLine("\n");
 
             Console.WriteLine(" Type een nummer in : ");
-            int Choose_nummer = int.Parse(Console.ReadLine());
 
 
-            if (Choose_nummer == 1)
-            {
-                Console.Clear();
-                Console.WriteLine("Gerecht 1        Prijs :3.5 ");
-                Console.WriteLine("Gerecht 2");
-                Console.WriteLine("Gerecht 3");
-                Console.WriteLine("Gerecht 4");
-                Console.WriteLine("Gerecht 5");
-                Console.WriteLine("Gerecht 6");
-                Console.WriteLine("Gerecht 7");
-                Console.WriteLine("Gerecht 8");
-                Console.WriteLine("Gerecht 9");
-                Console.WriteLine("Gerecht 10");
 
-            }
-            else if (Choose_nummer == 2)
+            string a = Console.ReadLine();
+
+            if (!int.TryParse(a, out int value))
             {
-                Console.Clear();
-                Console.WriteLine("Gerecht 1        Prijs :5.5 ");
-                Console.WriteLine("Gerecht 2");
-                Console.WriteLine("Gerecht 3");
-                Console.WriteLine("Gerecht 4");
-                Console.WriteLine("Gerecht 5");
-                Console.WriteLine("Gerecht 6");
-                Console.WriteLine("Gerecht 7");
-                Console.WriteLine("Gerecht 8");
-                Console.WriteLine("Gerecht 9");
-                Console.WriteLine("Gerecht 10");
-            }
-            else if (Choose_nummer == 3)
-            {
-                Console.Clear();
-                Console.WriteLine("Gerecht 1        Prijs :5.5 ");
-                Console.WriteLine("Gerecht 2");
-                Console.WriteLine("Gerecht 3");
-                Console.WriteLine("Gerecht 4");
-                Console.WriteLine("Gerecht 5");
-            }
-            else if (Choose_nummer == 4)
-            {
-                Console.Clear();
-                Console.WriteLine("Gerecht 1        Prijs :5.5 ");
-                Console.WriteLine("Gerecht 2");
-                Console.WriteLine("Gerecht 3");
-                Console.WriteLine("Gerecht 4");
-                Console.WriteLine("Gerecht 5");
-                Console.WriteLine("Gerecht 6");
+                Console.WriteLine("\n Probeer het opnieuw , vul een nummer in");
+                goto dot;
             }
             else
             {
-                Console.WriteLine("\n  Nummer mislukt,  Kies tussen de nummers 1 - 2 - 3 - 4");
-                goto dot;
+                int Choose_nummer = Convert.ToInt32(a);
+                if (Choose_nummer == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Gerecht 1        Prijs :3.5 ");
+                    Console.WriteLine("Gerecht 2");
+                    Console.WriteLine("Gerecht 3");
+                    Console.WriteLine("Gerecht 4");
+                    Console.WriteLine("Gerecht 5");
+                    Console.WriteLine("Gerecht 6");
+                    Console.WriteLine("Gerecht 7");
+                    Console.WriteLine("Gerecht 8");
+                    Console.WriteLine("Gerecht 9");
+                    Console.WriteLine("Gerecht 10");
+
+                    Console.WriteLine("Type @ om terug te gaan");
+                    string Back = Console.ReadLine();
+                    if (Back == "@")
+                    {
+                        Console.Clear();
+                        goto Begin;
+                    }
+
+                }
+                else if (Choose_nummer == 2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Gerecht 1        Prijs :5.5 ");
+                    Console.WriteLine("Gerecht 2");
+                    Console.WriteLine("Gerecht 3");
+                    Console.WriteLine("Gerecht 4");
+                    Console.WriteLine("Gerecht 5");
+                    Console.WriteLine("Gerecht 6");
+                    Console.WriteLine("Gerecht 7");
+                    Console.WriteLine("Gerecht 8");
+                    Console.WriteLine("Gerecht 9");
+                    Console.WriteLine("Gerecht 10");
+
+                    Console.WriteLine("Type @ om terug te gaan");
+                    string Back = Console.ReadLine();
+                    if (Back == "@")
+                    {
+                        Console.Clear();
+                        goto Begin;
+                    }
+                }
+                else if (Choose_nummer == 3)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Gerecht 1        Prijs :5.5 ");
+                    Console.WriteLine("Gerecht 2");
+                    Console.WriteLine("Gerecht 3");
+                    Console.WriteLine("Gerecht 4");
+                    Console.WriteLine("Gerecht 5");
+
+                    Console.WriteLine("Type @ om terug te gaan");
+                    string Back = Console.ReadLine();
+                    if (Back == "@")
+                    {
+                        Console.Clear();
+                        goto Begin;
+                    }
+                }
+                else if (Choose_nummer == 4)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Gerecht 1        Prijs :5.5 ");
+                    Console.WriteLine("Gerecht 2");
+                    Console.WriteLine("Gerecht 3");
+                    Console.WriteLine("Gerecht 4");
+                    Console.WriteLine("Gerecht 5");
+                    Console.WriteLine("Gerecht 6");
+
+                    Console.WriteLine("Type @ om terug te gaan");
+                    string Back = Console.ReadLine();
+                    if (Back == "@")
+                    {
+                        Console.Clear();
+                        goto Begin;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\n  Nummer mislukt,  Kies tussen de nummers 1 , 2 -, 3 - 4");
+                    goto dot;
+                }
             }
+                
+
+
+            //while (!int.TryParse(Console.ReadLine(), out Choose_nummer))
+            //{
+            //    Console.WriteLine("Fout");
+           // }
+
+                // int.Parse(Console.ReadLine());
+
+
+         
         }
     }
 
